676c3f02a79437102f6b9bb45e9f4f863e4c6876
Unicorn Barf <nolan.spence7@gmail.com>
2022-07-11 01:43:58 -0700

    Delete controller function now works.

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidV4 } = require('uuid');
 
 const getAllNotes = (req, res) => {
     const notes = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
@@ -11,11 +12,12 @@ const getAllNotes = (req, res) => {
     res.json(JSON.parse(notes));
 }
 
-const createNote = async (req, res) => {
+const createNote = (req, res) => {
     // Get user input from request body
     const newNote = req.body
-    console.log(req.body);
-    const notes = await JSON.parse(fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+    newNote.id = uuidV4(),
+    console.log(req.body.id);
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
         if (err) {
             return res.status(400).json({ err });
         }
@@ -39,8 +41,38 @@ const createNote = async (req, res) => {
 
 }
 
-const deleteNote = () => {
+const deleteNote = (req, res) => {
+    // get the note ID from req body
+    const { id } = req.params;
+    console.log(id);
+    // Read the database file and store in notes
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+            return res.status(400).json({ err });
+        }
+        return data;
+    }));
+    // remove note by id from notes array
+    notes.forEach((note, index) => {
+        if (note.id === id) {
+            notes.splice(index, 1);
+            return
+        }
+    });
     
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
+        if (err) {
+            return res.status(400).json({ err });
+        }
+        else {
+            const response = {
+                status: 'success',
+                body: newNote,
+            };
+        
+            res.json(response);
+        }
+    });
 }
 
 module.exports = {
